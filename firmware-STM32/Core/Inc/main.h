0baa719dc999711325dd3b58bcad115967fe6589
Tom <mr@tomclement.nl>
2020-03-18 21:12:26 +0100

    Fixed an tweaked button driver

--- firmware-STM32/Core/Inc/main.h
+++ firmware-STM32/Core/Inc/main.h
@@ -59,7 +59,7 @@ void Error_Handler(void);
 
 /* Private defines -----------------------------------------------------------*/
 /* USER CODE BEGIN Private defines */
-#define BUTTON_TICK 50
+#define BUTTON_TICK 5
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
