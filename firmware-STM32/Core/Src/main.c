85fb47fb21c607d30f4c1ad438c108551ac62b30
Tom <mr@tomclement.nl>
2020-03-21 18:21:33 +0100

    Changed oscillator to 16MHz

--- firmware-STM32/Core/Src/main.c
+++ firmware-STM32/Core/Src/main.c
@@ -197,7 +197,7 @@ void SystemClock_Config(void)
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV2;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
