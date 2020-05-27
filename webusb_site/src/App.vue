0d81b03b48685108d4d035dc14b1717ec4447264
DESKTOP-V6DBGP6\IPLogged <mr@tomclement.nl>
2020-05-27 23:50:57 +0200

    Added jstree Vue component, small fixes

--- webusb_site/src/App.vue
+++ webusb_site/src/App.vue
@@ -1,56 +1,5 @@
 <template>
   <div class="flexible-content">
-    <!--Navbar-->
-<!--    <mdb-navbar class="flexible-navbar white" light position="top" scrolling>-->
-<!--      <mdb-navbar-brand href="https://mdbootstrap.com/docs/vue/" target="_blank"-->
-<!--        >MDB</mdb-navbar-brand-->
-<!--      >-->
-<!--      <mdb-navbar-toggler>-->
-<!--        <mdb-navbar-nav left>-->
-<!--          <mdb-nav-item to="/" waves-fixed active class="active"-->
-<!--            >Home</mdb-nav-item-->
-<!--          >-->
-<!--          <mdb-nav-item-->
-<!--            href="https://mdbootstrap.com/docs/vue/getting-started/quick-start/"-->
-<!--            waves-fixed-->
-<!--            >About MDB</mdb-nav-item-->
-<!--          >-->
-<!--          <mdb-nav-item-->
-<!--            href="https://mdbootstrap.com/docs/vue/getting-started/download/"-->
-<!--            waves-fixed-->
-<!--            >Free download</mdb-nav-item-->
-<!--          >-->
-<!--          <mdb-nav-item-->
-<!--            href="https://mdbootstrap.com/education/bootstrap/"-->
-<!--            waves-fixed-->
-<!--            >Free tutorials</mdb-nav-item-->
-<!--          >-->
-<!--        </mdb-navbar-nav>-->
-<!--        <mdb-navbar-nav right>-->
-<!--          <mdb-nav-item href="#!" waves-fixed-->
-<!--            ><mdb-icon fab class="text-black" icon="facebook-square"-->
-<!--          /></mdb-nav-item>-->
-<!--          <mdb-nav-item href="#!" waves-fixed-->
-<!--            ><mdb-icon fab icon="twitter"-->
-<!--          /></mdb-nav-item>-->
-<!--          <mdb-nav-item-->
-<!--            href="https://github.com/mdbootstrap/bootstrap-material-design"-->
-<!--            waves-fixed-->
-<!--            class="border border-light rounded mr-1"-->
-<!--            target="_blank"-->
-<!--            ><mdb-icon fab icon="github" class="mr-2" />MDB GitHub-->
-<!--          </mdb-nav-item>-->
-<!--          <mdb-nav-item-->
-<!--            href="https://mdbootstrap.com/products/vue-ui-kit/"-->
-<!--            waves-fixed-->
-<!--            class="border border-light rounded"-->
-<!--            target="_blank"-->
-<!--            ><mdb-icon icon="gem" far class="mr-2" />Go Pro-->
-<!--          </mdb-nav-item>-->
-<!--        </mdb-navbar-nav>-->
-<!--      </mdb-navbar-toggler>-->
-<!--    </mdb-navbar>-->
-    <!--/.Navbar-->
     <!-- Sidebar -->
     <div class="sidebar-fixed position-fixed">
       <a class="logo-wrapper"
@@ -82,7 +31,7 @@
         <router-view></router-view>
       </div>
       <ftr color="primary-color-dark" class="text-center font-small darken-2">
-        <div class="pb-4">
+        <div class="pb-4 pt-4">
           <a href="#"><mdb-icon fab icon="facebook-square" class="mr-3"/></a>
           <a href="#"><mdb-icon fab icon="twitter" class="mr-3"/></a>
           <a href="#"><mdb-icon fab icon="youtube" class="mr-3"/></a>
@@ -92,10 +41,6 @@
           <a href="#"><mdb-icon fab icon="github" class="mr-3"/></a>
           <a href="#"><mdb-icon fab icon="codepen" class="mr-3"/></a>
         </div>
-        <p class="footer-copyright mb-0 py-3 text-center">
-          &copy; {{ new Date().getFullYear() }} Copyright:
-          <a href="https://mdbootstrap.com/docs/vue/"> MDBootstrap.com </a>
-        </p>
       </ftr>
     </main>
   </div>
--- webusb_site/src/components/Programming.vue
+++ webusb_site/src/components/Programming.vue
@@ -3,35 +3,43 @@
     <mdb-card class="mb-4">
       <mdb-card-body>
         <button onclick="connect()">Connect</button>
-        <h1>WebUSB Development</h1>
-        <div id="wrapper">
-          <input id="file-input" type="file" name="name" style="display: none;" />
-
-          <div id="icons">
-            <button class="btn" title="Delete file" v-on:click="trash_ui()"><i class="fa fa-trash"></i></button>
-            <button class="btn" title="Save file" onclick="save_ui()"><i class="fa fa-save"></i></button>
-            <button class="btn" title="Make folder" onclick="mkdir_ui()"><i class="fa fa-folder-plus"></i></button>
-            <button class="btn" title="Make file" onclick="mkfile_ui()"><i class="fa fa-file"></i></button>
-            <button class="btn" title="Sync" onclick="sync_ui()"><i class="fa fa-sync"></i></button>
-            <button class="btn" title="Rename file" onclick="rename_ui()"><i class="far fa-file-alt"></i></button>
-            <button class="btn" title="Upload file" onclick="upload_ui()"><i class="fas fa-upload"></i></button>
-            <button class="btn" title="Run file" onclick="startapp_ui()"><i class="fas fa-play"></i></button>
-            <button class="btn" title="Copy file" onclick="copy_ui()"><i class="fas fa-copy"></i></button>
-            <button class="btn" title="Cut file" onclick="cut_ui()"><i class="fas fa-cut"></i></button>
-            <button class="btn" title="Paste file" id="paste_btn" onclick="paste_ui()" disabled><i class="fas fa-paste"></i></button>
-          </div>
-          <div id="filebrowser" class="filebrowser"></div>
-
-          <editor v-model="content" lang="python" theme="monokai" height="500" @init="initEditor"></editor>
-        </div>
+        <section>
+        <mdb-row>
+          <mdb-col>
+            <mdb-row>
+              <mdb-col>
+                <mdb-btn color="gray" size="sm" title="Delete file" v-on:click="trash_ui()" icon="trash"></mdb-btn>
+                <mdb-btn color="gray" size="sm" title="Save file" v-on:click="save_ui()" icon="save"></mdb-btn>
+                <mdb-btn color="gray" size="sm" title="Make folder" v-on:click="mkdir_ui()" icon="folder-plus"></mdb-btn>
+                <mdb-btn color="gray" size="sm" title="Make file" v-on:click="mkfile_ui()" icon="file"></mdb-btn>
+                <mdb-btn color="gray" size="sm" title="Sync" v-on:click="sync_ui()" icon="sync"></mdb-btn>
+                <mdb-btn color="gray" size="sm" title="Rename file" v-on:click="rename_ui()" icon="file-alt"></mdb-btn>
+                <mdb-btn color="gray" size="sm" title="Upload file" v-on:click="upload_ui()" icon="upload"></mdb-btn>
+                <mdb-btn color="gray" size="sm" title="Run file" v-on:click="startapp_ui()" icon="play"></mdb-btn>
+                <mdb-btn color="gray" size="sm" title="Copy file" v-on:click="copy_ui()" icon="copy"></mdb-btn>
+                <mdb-btn color="gray" size="sm" title="Cut file" v-on:click="cut_ui()" icon="cut"></mdb-btn>
+                <mdb-btn color="gray" size="sm" title="Paste file" icon="paste" id="paste_btn" v-on:click="paste_ui()"></mdb-btn>
+              </mdb-col>
+            </mdb-row>
+            <mdb-row class="mt-3">
+              <mdb-col sm="6" md="4" lg="3">
+                <v-jstree :data="files" multiple allow-batch whole-row @item-click="itemClick"></v-jstree>
+              </mdb-col>
+              <mdb-col sm="6" md="8" lg="9">
+                <editor v-model="content" lang="python" theme="monokai" height="500" @init="initEditor"></editor>
+              </mdb-col>
+            </mdb-row>
+          </mdb-col>
+        </mdb-row>
+        </section>
       </mdb-card-body>
     </mdb-card>
   </section>
 </template>
 
 <script>
-import { mdbCard, mdbCardBody } from 'mdbvue';
-
+  import {mdbBtn, mdbCard, mdbCardBody, mdbCol, mdbRow} from 'mdbvue';
+import VJstree from 'vue-jstree';
 import {editor, trash_ui, readSingleFile} from '../editor';
 import {treechanged, fetch_dir} from '../webusb';
 import * as $ from 'jquery';
@@ -44,8 +52,12 @@ import * as ace_editor from 'vue2-ace-editor';
 export default {
   name: 'Programming',
   components: {
+    mdbBtn,
+    mdbRow,
+    mdbCol,
     mdbCard,
     mdbCardBody,
+    VJstree,
     editor:ace_editor
   },
   methods: {
@@ -65,22 +77,98 @@ export default {
       //         }
       //     });
     },
+    itemClick (node) {
+      console.log(node.model.text + ' clicked !')
+    },
     trash_ui: trash_ui
   },
   data () {
     return {
-      content:""
+      content:"",
+      files: [
+        {
+          "text": "Same but with checkboxes",
+          "children": [
+            {
+              "text": "initially selected",
+              "selected": true
+            },
+            {
+              "text": "custom icon",
+              "icon": "fa fa-warning icon-state-danger"
+            },
+            {
+              "text": "initially open",
+              "icon": "fa fa-folder icon-state-default",
+              "opened": true,
+              "children": [
+                {
+                  "text": "Another node"
+                }
+              ]
+            },
+            {
+              "text": "custom icon",
+              "icon": "fa fa-warning icon-state-warning"
+            },
+            {
+              "text": "disabled node",
+              "icon": "fa fa-check icon-state-success",
+              "disabled": true
+            }
+          ]
+        },
+        {
+          "text": "Same but with checkboxes",
+          "opened": true,
+          "children": [
+            {
+              "text": "initially selected",
+              "selected": true
+            },
+            {
+              "text": "custom icon",
+              "icon": "fa fa-warning icon-state-danger"
+            },
+            {
+              "text": "initially open",
+              "icon": "fa fa-folder icon-state-default",
+              "opened": true,
+              "children": [
+                {
+                  "text": "Another node"
+                }
+              ]
+            },
+            {
+              "text": "custom icon",
+              "icon": "fa fa-warning icon-state-warning"
+            },
+            {
+              "text": "disabled node",
+              "icon": "fa fa-check icon-state-success",
+              "disabled": true
+            }
+          ]
+        },
+        {
+          "text": "And wholerow selection"
+        }
+      ]
     }
   }
 }
 </script>
 
 <style scoped>
-.profile-card-footer {
-  background-color: #F7F7F7 !important;
-  padding: 1.25rem;
-}
-.card.card-cascade .view {
-  box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.15), 0 3px 12px 0 rgba(0, 0, 0, 0.15);
-}
+  .profile-card-footer {
+    background-color: #F7F7F7 !important;
+    padding: 1.25rem;
+  }
+  .card.card-cascade .view {
+    box-shadow: 0 3px 10px 0 rgba(0, 0, 0, 0.15), 0 3px 12px 0 rgba(0, 0, 0, 0.15);
+  }
+  .btn.btn-sm {
+    padding: .5rem .5rem !important;
+  }
 </style>
