1d9f545e05de8db1d450cd3d88dfb029481ff364
Tom <mr@tomclement.nl>
2020-07-12 13:43:24 +0200

    Updated flash script

--- gold_release_firmware/batchflash.py
+++ gold_release_firmware/batchflash.py
@@ -1,9 +1,22 @@
 import sys, threading, time
-from os import system
+import subprocess
 from os.path import exists
 
 def find_device(number):
-    devices = ['/dev/ttyUSB%d' % number, '/dev/ttyACM%d' % number, '/dev/tty.usbmodem123456%d' % number, '/dev/tty.usbmodem%d' % number, '/dev/tty.usbmodem123456%d' % number]
+    devices = ['/dev/ttyUSB%d' % number,
+               '/dev/ttyACM%d' % number,
+               '/dev/tty.usbmodem123456%d' % number,
+               '/dev/tty.usbmodem%d' % number,
+               '/dev/tty.usbmodem1%d' % number,
+               '/dev/tty.usbmodem2%d' % number,
+               '/dev/tty.usbmodem3%d' % number,
+               '/dev/tty.usbmodem4%d' % number,
+               '/dev/tty.usbmodem5%d' % number,
+               '/dev/tty.usbmodem6%d' % number,
+               '/dev/tty.usbmodem7%d' % number,
+               '/dev/tty.usbmodem8%d' % number,
+               '/dev/tty.usbmodem9%d' % number,
+               '/dev/tty.usbmodem123456%d' % number]
     for device in devices:
         if exists(device):
             return device
@@ -17,7 +30,8 @@ def flash_daemon(number):
         if device is not None:
             print('Flashing %s' % device)
             sys.stdout.flush()
-            system('esptool.py -p %s --baud 2000000 write_flash 0x1e1000 initial_fs.zip 0xd000 ota_data_initial.bin 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 campzone2020_16MB.bin' % device)
+            cmd = 'esptool.py -p %s --baud 2000000 write_flash 0x9000 nvs.bin 0x1e1000 initial_fs.zip 0xd000 ota_data_initial.bin 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 campzone2020_16MB.bin' % device
+            subprocess.run(cmd.split(' '))
 
             # Give system time to adjust
             time.sleep(1)
@@ -28,7 +42,7 @@ def flash_daemon(number):
 
         time.sleep(0.1)
 
-for i in range(0,60):
+for i in range(0,10):
     threading.Thread(target=flash_daemon, args=(i,)).start()
 
 while True:
