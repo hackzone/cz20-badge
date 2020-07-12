81dc85842546e4f994f5c227b59afec3fdcf3f1f
Tom <mr@tomclement.nl>
2020-07-12 13:08:28 +0200

    Fixing batch badge flash script

--- gold_release_firmware/batchflash.py
+++ gold_release_firmware/batchflash.py
@@ -10,14 +10,14 @@ def find_device(number):
     return None
 
 def flash_daemon(number):
-    print('Starting flash thread for %s' % device)
+    print('Starting flash thread for %s' % number)
     sys.stdout.flush()
     while True:
         device = find_device(number)
         if device is not None:
             print('Flashing %s' % device)
             sys.stdout.flush()
-            system('env CONFIG_ESPTOOLPY_PORT_X=%s esptool.py --baud 250000 write_flash 0x1e1000 initial_fs.zip 0xd000 ota_data_initial.bin 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 generic_4MB.bin' % device)
+            system('env CONFIG_ESPTOOLPY_PORT_X=%s esptool.py --baud 250000 write_flash 0x1e1000 initial_fs.zip 0xd000 ota_data_initial.bin 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 campzone2020_16MB.bin' % device)
 
             # Give system time to adjust
             time.sleep(1)
