5d071afa41ecfe6de2a599d0a62c58077e1c966b
Tom <mr@tomclement.nl>
2020-07-12 13:55:57 +0200

    Updated flash script

--- gold_release_firmware/batchflash.py
+++ gold_release_firmware/batchflash.py
@@ -30,7 +30,7 @@ def flash_daemon(number):
         if device is not None:
             print('Flashing %s' % device)
             sys.stdout.flush()
-            cmd = 'esptool.py -p %s --baud 2000000 write_flash 0x9000 nvs.bin 0x1e1000 initial_fs.zip 0xd000 ota_data_initial.bin 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 campzone2020_16MB.bin' % device
+            cmd = 'esptool.py -p %s --baud 2000000 write_flash 0x1e1000 initial_fs.zip 0xd000 ota_data_initial.bin 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 campzone2020_16MB.bin' % device
             subprocess.run(cmd.split(' '))
 
             # Give system time to adjust
